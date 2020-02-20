bc2599f4f67582924d3d1e8cc85d493fe42e7429
elisha <ebehelisha@gmail.com>
2020-02-20 17:10:35 +0000

    made some changes

--- ship.py
+++ ship.py
@@ -1,20 +1,25 @@
 import pygame
+from settings import Settings
 class Ship:
     """A class to manag the ship"""
 
     def __init__(self, ai_game):
         """Initialize the ship and get it starting location"""
+        self.settings = Settings()
+
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
+        
         # Load the ship
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
 
         # Starting each ship at the bottom 
-
         self.rect.midbottom = self.screen_rect.midbottom
-
+        # Store a decimal value for the ship's horizontal postion
+        self.x = float(self.rect.x)
         self.moving_right = False
         self.moving_left = False
 
@@ -25,7 +30,9 @@ class Ship:
 
     def update(self):
         """ Updating the ship's position based on movement flag. """
-        if self.moving_right == True:
-            self.rect.x += 1
+        if self.moving_right:
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
